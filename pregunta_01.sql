4ffbb15e4caee246bbd824faa27766527501b88b
daniela-giraldod <112145954+daniela-giraldod@users.noreply.github.com>
2022-10-19 03:06:45 -0500

    Update pregunta_01.sql

--- pregunta_01.sql
+++ pregunta_01.sql
@@ -17,3 +17,31 @@
 --
 --  >>> Escriba su codigo a partir de este punto <<<
 --
+
+import pandas as pd
+
+tabla1 = pd.read_csv(
+    "tbl1.csv",
+    sep=",",
+    thousands=None,
+    header=None,
+    decimal=".",
+    encoding="latin-1",
+    names=["K0", "K1", "c12", "c13","c14","c15","c16"]
+)
+#
+
+tabla1.to_sql(name="tabla1", con=conn, if_exists="replace")  ### hacer conexion de archivo com sql
+
+#tabla1[0:1]
+
+
+cur.execute(
+    """
+SELECT 
+    sum(c12)
+
+FROM
+    tabla1
+"""
+).fetchall()
